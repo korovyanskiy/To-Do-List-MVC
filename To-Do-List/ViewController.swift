29101e58509d13a0adc99ce0e7566077efc7bb52
korovyanskiy <korovyn@list.ru>
2022-05-26 11:10:19 +0300

    Initial Commit

---
+++ To-Do-List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-Do-List
+//
+//  Created by Igor Korovyanskiy on 26.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
